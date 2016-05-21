d8fe0e8f9c151de44b35291828330115455e3a1a
Antonio Carlos Falcão Petri <falcaopetri@gmail.com>
2016-05-21 15:47:00 -0300

    Added delay to process starting and ordered output

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
 import json
+import time
+from threading import Timer
 
 with open("example.cfg") as f:
     config = json.load(f)
@@ -13,38 +15,61 @@ print_interval = config["print_interval"]
 
 processes = []
 
-for process_cfg in config["processes"]:
+def run_subprocess(process_cfg):
     data = process_cfg["data"]
     types = process_cfg["types"]
-    id = len(processes)
+    id = process_cfg["id"]
 
     process = subprocess.Popen(cmd.format(exe, id, print_interval, data, *types),
                                     shell=True, stdout=subprocess.PIPE)
+    process.id = id
     processes.append(process)
+
+for i in range(len(config["processes"])):
+    process_cfg = config["processes"][i]
+    process_cfg["id"] = i
+
+    delay = process_cfg.get("delay", 0)
+
+    Timer(delay, run_subprocess, [process_cfg]).start()
 
 labels = [ "Process ID", "Tempo decorrido", "User time", "System time",
                 "Porcentagem de uso da cpu", "Porcentagem de uso da cpu por thread" ]
 
-n_active_processes = len(processes)
+n_processes = len(config["processes"])
+# n_active_processes = len(config["processes"])
 
 output_file = open("output.csv", "w")
 
+iteration = 0
+
+# TODO exit after all processes have finished
 # Reference: http://www.cyberciti.biz/faq/python-run-external-command-and-get-output/
-while n_active_processes != 0:
+while True:
+    ordered_buff = ["0"] * n_processes
+
     for process in processes:
         output = process.stdout.readline()
 
         if output == '' and process.poll() is not None:
-            n_active_processes -= 1
+            # n_active_processes -= 1
             processes.remove(process)
             continue
 
         if output:
             values = output.strip().split(" ")
-            output_file.write(values[5])
-            output_file.write(",")
+            id = process.id
+            ordered_buff[id] = values[5]
 
             for i in range(len(labels)):
                 print "{}: {}".format(labels[i], values[i])
+
+    output_file.write(str(iteration))
+    iteration += 1
+
+    for v in ordered_buff:
+        output_file.write(",")
+        output_file.write(v)
+
     output_file.write("\n")
     output_file.flush()
