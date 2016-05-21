e025397c9965e1fd41eaa27bb70ae96ae567a108
Antonio Carlos Falcão Petri <falcaopetri@gmail.com>
2016-05-21 09:01:05 -0300

    Process from config file Processes configuration is read from config file on JSON format

--- run.py
+++ run.py
@@ -1,26 +1,34 @@
 import subprocess
 import sys
+import json
 
-n_processes = 5
+with open("example.cfg") as f:
+    config = json.load(f)
+
 exe = "./a.out"
 args = "{} {} {} {} {} {} {}"
 cmd = "{} " + args
 
+print_interval = config["print_interval"]
+
 processes = []
 
-for i in range(n_processes):
-    process = subprocess.Popen(cmd.format(exe, i, 2, 10000, 5, 0, 0, 0),
+for process_cfg in config["processes"]:
+    data = process_cfg["data"]
+    types = process_cfg["types"]
+    id = len(processes)
+
+    process = subprocess.Popen(cmd.format(exe, id, print_interval, data, *types),
                                     shell=True, stdout=subprocess.PIPE)
     processes.append(process)
 
 labels = [ "Process ID", "Tempo decorrido", "User time", "System time",
-                "Porcentagem de uso da cpu", "Porcentagem de uso da cpu por thread"]
+                "Porcentagem de uso da cpu", "Porcentagem de uso da cpu por thread" ]
 
-n_active_processes = n_processes
+n_active_processes = len(processes)
 
 # Reference: http://www.cyberciti.biz/faq/python-run-external-command-and-get-output/
 while n_active_processes != 0:
-    print n_active_processes
     for process in processes:
         output = process.stdout.readline()
 
